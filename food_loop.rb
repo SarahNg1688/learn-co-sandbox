9b36a647b91f6ccdcbfa4a167e2352b39d132d04
Sarah Ng <agbtr101a@gmail.com>
2018-08-07 18:25:45 +0000

    Done.

---
+++ food_loop.rb
@@ -0,0 +1,8 @@
+def hungry_sarah
+scallops_shrimps_spaghetti = 0
+until scallops_shrimps_spaghetti == 100
+puts "SCALLOPS SHRIMPS AND SPAGHETTI IS THE BEST"
+scallops_shrimps_spaghetti += 1
+end
+end
+hungry_sarah
