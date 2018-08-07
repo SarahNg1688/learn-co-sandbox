7df79d58bf30fba8a0421a7d914c7b36d0cdba9d
Sarah Ng <agbtr101a@gmail.com>
2018-08-07 20:19:24 +0000

    Done.

---
+++ arrayss_practice.rb
@@ -0,0 +1,5 @@
+sneaker_brands=["Nike", "Adidas", "Vans", "Skechers"]
+#when making arrays use square brackets
+#the comma should be outside of the quotation marks
+sneaker_brands.push("converse")
+puts sneaker_brands.size
